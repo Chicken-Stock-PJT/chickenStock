86e442255156dcf5de8d53482f2d3b57c2885658
여현승 <yhs10179@gmail.com>
2025-04-28 12:47:35 +0900

    feat: 메인페이지 - 배너
    - 서비스 설명 메시지 + 로고 추가
    - 버튼 클릭 시 주식 목록 페이지로 이동
    
    S12P31A106-54

---
+++ FE/chickenstock-web/src/features/mainpage/ui/Banner.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/shared/libs/ui/button";
+import logo from "@/assets/logoImg.svg";
+import { useNavigate } from "react-router-dom";
+
+const Banner = () => {
+  const navigate = useNavigate();
+  return (
+    <section className="bg-gradient-to-b from-yellow-50 to-white py-16">
+      <div className="container mx-auto max-w-[1200px] px-4">
+        <div className="flex flex-col items-center gap-8 md:flex-row">
+          <div className="space-y-6 text-left md:w-1/2">
+            <h1 className="text-4xl/2 font-bold leading-tight md:text-5xl">
+              투자가 처음이라면,
+              <br />
+              <span className="mt-2 block text-yellow-600">
+                ChickenStock
+                <span className="text-black">과 함께</span>
+              </span>
+            </h1>
+            <p className="text-lg text-muted-foreground">
+              실제 투자가 두려운 분들을 위한 모의투자 플랫폼. <br />
+              <span className="text-lg text-muted-foreground">
+                재미있게 투자를 배우고 실전 감각을 키워보세요.
+              </span>
+            </p>
+
+            <Button size="lg" className="font-bold" onClick={() => void navigate("/stocks/005930")}>
+              모의투자 시작하기
+            </Button>
+          </div>
+          <div className="mx-auto md:w-1/3">
+            <img src={logo} alt="ChickenStock 모의투자 플랫폼" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
