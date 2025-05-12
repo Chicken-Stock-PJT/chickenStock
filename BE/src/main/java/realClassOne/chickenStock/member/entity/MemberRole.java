8eaa7171cc4c2b549ba0f005e40d2e14b47b9923
kimjeongwoo <zwjddn1105@naver.com>
2025-05-12 10:53:19 +0900

    chore: BE폴더삭제

--- BE/src/main/java/realClassOne/chickenStock/member/entity/MemberRole.java
+++
@@ -1,7 +0,0 @@
-package realClassOne.chickenStock.member.entity;
-
-public enum MemberRole {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_AI
-}
--- BE/src/main/java/realClassOne/chickenStock/stock/dto/response/InitializeMoneyResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package realClassOne.chickenStock.stock.dto.response;
-
-public record InitializeMoneyResponseDTO(
-        String status,
-        String message,
-        Long memberId
-) {}
