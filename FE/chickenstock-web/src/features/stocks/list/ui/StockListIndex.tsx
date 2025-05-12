9d43d4e964b759fe212e5bc959c77e29b919a586
kimjeongwoo <zwjddn1105@naver.com>
2025-05-12 10:10:27 +0900

    feat: 홈화면 메뉴 sticky처리

--- FE/chickenstock-web/src/features/stocks/list/ui/StockListIndex.tsx
+++ FE/chickenstock-web/src/features/stocks/list/ui/StockListIndex.tsx
@@ -32,7 +32,7 @@ const StockListIndex = ({ rankingType }: StockListIndexProps) => {
   };
 
   return (
-    <div className="flex items-center justify-between rounded-t-lg bg-primary-50 p-4 text-gray-700">
+    <div className="sticky top-0 z-10 flex items-center justify-between rounded-t-lg bg-primary-50 p-4 text-gray-700">
       {getColumns().map((column) => (
         <div
           key={column.id}
